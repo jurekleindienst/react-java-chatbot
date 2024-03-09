d86ecbff839df8f0db7fae657654a4263da59b62
jurekleindienst <info@jurekleindienst.com>
2024-03-09 21:31:27 +0100

    added animations, images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,24 @@ import React, { useEffect, useState } from "react";
 import CategoryList from "./components/CategoryList";
 import QuestionList from "./components/QuestionList";
 import './styles/App.css';
+import chatbotIcon from './assets/images/turtle.png';
 
 function App() {
     const [selectedCategory, setSelectedCategory] = useState(null);
     const [selectedQuestion, setSelectedQuestion] = useState(null);
     const [chatHistory, setChatHistory] = useState([]);
     const [chatStarted, setChatStarted] = useState(false);
+    const [isChatOpen,setIsChatOpen] = useState(false);
     const greetingText = "Hi, it's great to see you!";
     const instructionText1 = "What information are you looking for? Please use the navigation below.";
+
+    const openChat = () => {
+        setIsChatOpen(true);
+    }
+
+    const closeChat = () => {
+        setIsChatOpen(false);
+    }
 
     const addInstructionToHistory = () => {
         setChatHistory(history => [
@@ -56,41 +66,52 @@ function App() {
 
     return (
         <div className="app">
-            <div className="chat-header">Camping Šobec</div>
+            {!isChatOpen && (
+                <button className="chat-popup" onClick={openChat}>ASK A QUESTION!</button>
+            )}
 
-            <div className="chat">
-                <div className="message-sender-intro">Chatbot</div>
-                <div className="greeting">
-                    <p>{greetingText} {String.fromCodePoint(0x1F44B)}</p>
-                </div>
-                <div className="greeting">
-                    <p>{instructionText1}</p>
+            <div className={`chat-container ${isChatOpen ? 'chat-container-visible' : ''}`}>
+                <div className="chat-header">
+                    <img src={chatbotIcon} className="chatbot-header-icon" alt="Chatbot"/>
+                    <span>Tomi</span>
+                    <button className="close-chat" onClick={closeChat}>×</button>
                 </div>
 
-                {chatHistory.map((entry, index) => (
-                    <div className={`chat-entry ${entry.sender}`} key={index}>
-                        <div className={`message-sender ${entry.sender}`}>
-                            {entry.sender === 'system' ? 'Chatbot' : 'You'}
-                        </div>
-                        <div className="message-content">
-                            <p>{entry.text}</p>
-                        </div>
+                <div className="chat">
+                    <div className="message-sender-intro">Tomi</div>
+                    <div className="greeting">
+                        <p>{greetingText} {String.fromCodePoint(0x1F44B)}</p>
                     </div>
-                ))}
+                    <div className="greeting">
+                        <p>{instructionText1}</p>
+                    </div>
 
-                {!chatStarted && <CategoryList onSelectCategory={handleSelectCategory} />}
+                    {chatHistory.map((entry, index) => (
+                        <div className={`chat-entry ${entry.sender}`} key={index}>
+                            <div className={`message-sender ${entry.sender}`}>
+                                <div className="message-sender-chat">
+                                    {entry.sender === 'system' ? 'Tomi' : 'You'}
+                                </div>
+                            </div>
+                            <div className="message-content">
+                                <p>{entry.text}</p>
+                            </div>
+                        </div>
+                    ))}
 
-                {chatStarted && selectedCategory && !selectedQuestion && (
-                    <QuestionList categoryId={selectedCategory.id} onSelectQuestion={handleSelectQuestion} />
-                )}
+                    {!chatStarted && <CategoryList onSelectCategory={handleSelectCategory} />}
+
+                    {chatStarted && selectedCategory && !selectedQuestion && (
+                        <QuestionList categoryId={selectedCategory.id} onSelectQuestion={handleSelectQuestion} />
+                    )}
+                </div>
+
+                <div className="chat-footer">
+                    <button className="reset-button" onClick={handleResetChat}>
+                        Ask a new question
+                    </button>
+                </div>
             </div>
-
-            <div className="chat-footer">
-                <button className="reset-button" onClick={handleResetChat}>
-                    Ask a new question
-                </button>
-            </div>
-
         </div>
     );
 }
