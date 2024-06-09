742360cb26e2b7b8a5d37cdb8e388373fc3e57a3
jurekleindienst <info@jurekleindienst.com>
2024-06-09 20:17:47 +0200

    css (header, moving arrows)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -26,14 +26,35 @@ function App() {
     // Depending on the value of 'currentView', either the 'CategoryList' or the 'QuestionList' is rendered.
     return (
         <div className="app-container">
-            {currentView === 'categoryList' && <CategoryList onSelectCategory={handleSelectCategory} />}
-            {currentView === 'questionList' && selectedCategoryId && (
-                <div>
-                    {/* A back button, that allows navigating back to the category list from the questions view. */}
-                    <button onClick={handleBackToCategories}>Back to Categories</button>
-                    <QuestionList categoryId={selectedCategoryId} categoryName={selectedCategoryName}/>
+            <div className="header">
+                {currentView === 'questionList' && selectedCategoryId && (
+                    <div className="back-to-categories-container">
+                        {/* A back button, that allows navigating back to the category list from the questions view. */}
+                        <button className="back-to-categories-button" onClick={handleBackToCategories}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.7" d="m14 18-6-6 6-6"></path>
+                            </svg>
+                        </button>
+                    </div>
+                )}
+                <div className="close-chatbot-container">
+                    {/* Button to close the chatbot */}
+                    <button className="close-chatbot-button">
+                        <svg width="24" height="24" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 10 L30 30 M30 10 L10 30" stroke="currentColor" stroke-width="3" stroke-linecap="round"/>
+                        </svg>
+                    </button>
                 </div>
-            )}
+
+            </div>
+            <div className="faq-container">
+                {currentView === 'categoryList' && <CategoryList onSelectCategory={handleSelectCategory} />}
+                {currentView === 'questionList' && selectedCategoryId && (
+                    <div>
+                        <QuestionList categoryId={selectedCategoryId} categoryName={selectedCategoryName}/>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- frontend/src/components/QuestionList.js
+++ frontend/src/components/QuestionList.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
-const QuestionList = ({categoryId, categoryName}) => {
+const QuestionList = ({categoryId}) => {
     // Set and Save questions of currently selected category.
     const [questions, setQuestions] = useState([]);
     // Set and Save current active question => which answer should be displayed.
@@ -23,12 +23,17 @@ const QuestionList = ({categoryId, categoryName}) => {
     // Clicking the button toggles the visibility of the corresponding answer just below it.
     return (
         <div className="question-list-container">
-            <p>{categoryName}</p>
             {questions.map((question) => (
-                <div className="question-item" key={question.id}>
+                <div key={question.id}>
                     {/* Each question can be clicked to toggle its answer's visibility. */}
                     <button className="question-button" onClick={() => toggleQuestion(question.id)} >
                         {question.question}
+                        {/* Arrow SVG, when question is active, it will have 'rotate-arrow'. */}
+                        <svg
+                            className={activeQuestionId === question.id ? "rotate-arrow" : ""}
+                            xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="#0077BD">
+                            <path d="M5.42773 4.70898C5.46387 4.85254 5.53809 4.98828 5.65039 5.10059L8.54932 8L5.64893 10.9004C5.31689 11.2324 5.31689 11.7705 5.64893 12.1025C5.98096 12.4336 6.51904 12.4336 6.85107 12.1025L10.3516 8.60059C10.5591 8.39355 10.6367 8.10449 10.585 7.83691C10.5537 7.67578 10.4761 7.52246 10.3516 7.39844L6.85254 3.89941C6.52051 3.56738 5.98242 3.56738 5.65039 3.89941C5.43066 4.11816 5.35645 4.42871 5.42773 4.70898Z"></path>
+                        </svg>
                     </button>
                     {/* Conditionally render the answer section only if this question is the active question. */}
                     {activeQuestionId === question.id && (
